403055dbfc6f12e06ddd20a47b9d6f81e6f816ee
JohnsonNYC <jkow95@gmail.com>
2020-08-11 13:12:00 -0400

    changed the database from sqlite to postgresql

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
     def create
-        order =  Order.create(params.require(:order),permit(:user_id,:ice_cream_id))
+        order =  Order.create(params.require(:order).permit(:user_id,:ice_cream_id))
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_10_180425) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ice_creams", force: :cascade do |t|
     t.string "flavor"
     t.integer "rating"
